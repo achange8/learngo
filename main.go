3e200bc58fa12e4e832df3d4b65146c5b5e75639
achange8 <95342381+achange8@users.noreply.github.com>
2022-01-19 19:14:44 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	scrapper "learngo/scarpper"
+	"os"
 	"strings"
 
 	"github.com/labstack/echo"
 )
+
+const fileName string = "jobs.csv"
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handlescrape(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	return nil
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
